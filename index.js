64255e255580e49cee25afd92287e3665192ac55
Gemma Batlle <gembb15@gmail.com>
2022-10-03 11:38:27 +0200

    sesion-15

---
+++ index.js
@@ -0,0 +1,15 @@
+const nombre = "Gemma";
+const apellido = "Batlle";
+
+const obj = {
+    nom: nombre,
+    apell: apellido
+}
+
+const fecha = new Date();
+const fecha1 = fecha.getMinutes() + 2;
+// sessionStorage.setItem("objeto", JSON.stringify(obj));
+// localStorage.setItem("objeto", JSON.stringify(obj));
+
+// document.cookie = "objCookie=" + JSON.stringify(obj); "expires=" + JSON.stringify(fecha1);
+
